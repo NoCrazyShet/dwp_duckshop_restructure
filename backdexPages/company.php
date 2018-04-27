549e2c5e9d0b4d94ceb7c1e8ce930977dab0923b
NoCrazyShet <j.pettit51@gmail.com>
2018-04-27 13:43:13 +0200

    Image handling start, no functionality

--- backdexPages/company.php
+++ backdexPages/company.php
@@ -6,41 +6,51 @@ require_once("./backdexPageControllers/backdexCompanyController.php");
 <div class="row" style="margin-top: 50px;">
     <form class="col s12" name="lars" id="lars" method="POST" action="./backdex.php?action=update&page=company">
         <div class="card">
-                <div class="card-image">
-                    <img src="./images/top-duck.svg">
-                    <span class="card-title black-text">Click image to change</span>
-                </div>
-                <div class="card-content">
-            <div class="row">
-                <div class="input-field col s12 m12">
-                    <textarea id="logoText" name="logoText" class="materialize-textarea"><?php echo $compInfo['logoText'];?></textarea>
-                    <label for="logoText">Logo Text</label>
-                </div>
-            </div>
-            <div class="row">
-                <div class="input-field col s12 m12">
-                    <textarea id="aboutUs" name="aboutUs" class="materialize-textarea"><?php echo $compInfo['aboutUs'];?></textarea>
-                    <label for="aboutUs">About Us</label>
+            <div class="card-image">
+                <img src="./images/top-duck.svg">
+                <div class="file-field input-field">
+                    <div class="card-content">
+                        <div class="btn">
+                            <span>Change image</span>
+                            <input type="file">
+                        </div>
+                        <div class="file-path-wrapper">
+                            <input class="file-path validate" type="text" placeholder="Select a file to upload">
+                        </div>
+                    </div>
                 </div>
             </div>
-            <div class="row">
-                <div class="input-field col s12 m6">
-                    <textarea id="street" name="street" class="materialize-textarea"><?php echo $compInfo['street'];?></textarea>
-                    <label for="street">Street</label>
+            <div class="card-content">
+                <div class="row">
+                    <div class="input-field col s12 m12">
+                        <textarea id="logoText" name="logoText" class="materialize-textarea"><?php echo $compInfo['logoText'];?></textarea>
+                        <label for="logoText">Logo Text</label>
+                    </div>
                 </div>
-                <div class="input-field col s6 m3">
-                    <textarea id="streetNumber" name="streetNumber" class="materialize-textarea"><?php echo $compInfo['streetNumber'];?></textarea>
-                    <label for="streetNumber">Street number</label>
+                <div class="row">
+                    <div class="input-field col s12 m12">
+                        <textarea id="aboutUs" name="aboutUs" class="materialize-textarea"><?php echo $compInfo['aboutUs'];?></textarea>
+                        <label for="aboutUs">About Us</label>
+                    </div>
                 </div>
-                <div class="input-field col s6 m3">
-                    <textarea id="zipCode" name="zipCode" class="materialize-textarea"><?php echo $compInfo['zipCode'];?></textarea>
-                    <label for="zipCode">Zip Code</label>
+                <div class="row">
+                    <div class="input-field col s12 m6">
+                        <textarea id="street" name="street" class="materialize-textarea"><?php echo $compInfo['street'];?></textarea>
+                        <label for="street">Street</label>
+                    </div>
+                    <div class="input-field col s6 m3">
+                        <textarea id="streetNumber" name="streetNumber" class="materialize-textarea"><?php echo $compInfo['streetNumber'];?></textarea>
+                        <label for="streetNumber">Street number</label>
+                    </div>
+                    <div class="input-field col s6 m3">
+                        <textarea id="zipCode" name="zipCode" class="materialize-textarea"><?php echo $compInfo['zipCode'];?></textarea>
+                        <label for="zipCode">Zip Code</label>
+                    </div>
                 </div>
             </div>
+        <div class="card-action">
+             <button class="waves-effect waves-light btn-large" type="submit" name="submit">Update Company Information</button>
         </div>
-            <div class="card-action">
-                 <button class="waves-effect waves-light btn-large" type="submit" name="submit">Update Company Information</button>
-            </div>
     </form>
 </div>
 
