9fe3946d2427bc217458ffc9a5a05c1b4dcda44b
NoCrazyShet <j.pettit51@gmail.com>
2018-04-27 14:14:26 +0200

    Figured out styling of button alignment for dynamic logout

--- backdex.php
+++ backdex.php
@@ -24,9 +24,11 @@ $login = new loginController();
                     <li><a href="backdex.php?page=company">Edit Company</a></li>
                     <li><a href="index.php?page=products">Edit products</a></li>
                     <li><a href="index.php?page=login">Login to your account</a></li>
-                    <?php if(isset($_SESSION['userID'])){echo '<li><form action="index.php?page=yard&login=false" method="post" style="margin-bottom: 0;">';
+                    <?php if(isset($_SESSION['userID'])){
+                        echo '<li><form action="index.php?page=yard&login=false" method="post" style="margin-bottom: 0; display: inherit;">';
                         echo '<button type="submit" class="waves-effect waves-light btn">Log Out</button>';
-                        echo '</form></li>';} ?>
+                        echo '</form></li>';
+                    }?>
                 </ul>
             </div>
         </div>
