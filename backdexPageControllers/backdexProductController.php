4d32b60cc1ea3655a3b9786f8190c0fe9bf21dbf
Zeljko laptop <duranovic46@gmail.com>
2018-05-08 13:35:47 +0200

    Making progress on product page. foreach loop functional.

--- backdexPageControllers/backdexProductController.php
+++ backdexPageControllers/backdexProductController.php
@@ -1,3 +1,25 @@
 <?php
-
 $products = $db->runQuery("SELECT * FROM product", 'fetchAll', PDO::FETCH_ASSOC);
+$rowCount = 0;
+?>
+    <div class="row"> <?php
+foreach ($products as $row) {
+    ?>
+        <div class="col s6 m3">
+            <div class="card">
+                <div class="card-image">
+                    <img src="./images/<?php echo $row['productIMG'] ?>">
+                    <span class="card-title black-text"><?php echo $row['productName']?></span>
+                </div>
+                <div class="card-content">
+                    <?php echo $row['productName'] ?>
+                </div>
+                <div class="card-action">
+                    <a href="#">Something</a>
+                </div>
+            </div>
+        </div>
+    <?php
+     $rowCount++;
+    if ($rowCount % 4 == 0) echo '</div><div class="row">';
+}
--- test.sql
+++ test.sql
@@ -12,3 +12,24 @@ VALUES (1, 10, NULL, 'very nice product', 'yes 10', 99, 'very nice duck2');
 
 INSERT INTO product (categoryID, productRating, productIMG, productDescription, productStock, productPrice, productName)
 VALUES (2, 10, NULL, 'very nice product', 'yes 10', 99, 'very nice duck3');
+
+INSERT INTO product (categoryID, productRating, productIMG, productDescription, productStock, productPrice, productName)
+VALUES (2, 10, NULL, 'very nice product', 'yes 10', 99, 'very nice duck4');
+
+INSERT INTO product (categoryID, productRating, productIMG, productDescription, productStock, productPrice, productName)
+VALUES (2, 10, NULL, 'very nice product', 'yes 10', 99, 'very nice duck5');
+
+INSERT INTO product (categoryID, productRating, productIMG, productDescription, productStock, productPrice, productName)
+VALUES (2, 10, NULL, 'very nice product', 'yes 10', 99, 'very nice duck6');
+
+INSERT INTO product (categoryID, productRating, productIMG, productDescription, productStock, productPrice, productName)
+VALUES (2, 10, NULL, 'very nice product', 'yes 10', 99, 'very nice duck7');
+
+INSERT INTO product (categoryID, productRating, productIMG, productDescription, productStock, productPrice, productName)
+VALUES (2, 10, NULL, 'very nice product', 'yes 10', 99, 'very nice duck8');
+
+INSERT INTO product (categoryID, productRating, productIMG, productDescription, productStock, productPrice, productName)
+VALUES (2, 10, NULL, 'very nice product', 'yes 10', 99, 'very nice duck9');
+
+INSERT INTO product (categoryID, productRating, productIMG, productDescription, productStock, productPrice, productName)
+VALUES (2, 10, NULL, 'very nice product', 'yes 10', 99, 'very nice duck10');
