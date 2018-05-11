e3641abe3a41d7d0439d6242e233fcdc41375090
NoCrazyShet <j.pettit51@gmail.com>
2018-05-11 10:21:37 +0200

    Changed the create procedure added redirects

--- backdexPageControllers/backdexProductCreateController.php
+++ backdexPageControllers/backdexProductCreateController.php
@@ -1,6 +1,12 @@
 <?php
 
 
+if(!isset($_GET['case'])) {
+    $productCase = NULL;
+}elseif (isset($_GET['case'])) {
+    $productCase = $_GET['case'];
+}
+
 if(isset($_GET['action'])) {
     $action = $_GET['action'];
     if($action == 'create') {
@@ -15,6 +21,21 @@ if(isset($_GET['action'])) {
         $db->boundQuery("INSERT INTO product (productIMG, categoryID, productDescription, productStock, productPrice, productName) VALUES (:productIMG, :categoryID, :productDescription, :productStock, :productPrice, :productName)", $values);
         $id = $db->connection->lastInsertId();
 
-        redirect_to("./backdex.php?page=backdexProductsUpdate&id=$id");
+
+        redirect_to("./backdex.php?page=backdexProductsCreate&id=$id&case=picture");
+    } elseif ($action == "productImage") {
+        require_once ('./controllers/imageUploadController.php');
+        require_once("./controllers/imageResizer.php");
+        $values = array('productID' => $_GET['id']);
+        $updateProduct = $db->boundQuery("SELECT * FROM product WHERE productID=:productID", $values, 'fetch', PDO::FETCH_ASSOC);
+        $id = $updateProduct['productID'];
+        $selVal = array('productID' => $updateProduct['productID']);
+        $imgCnt = new imageUploadController();
+        $imgCnt->imageUpload("SELECT productID FROM product WHERE productID = :productID", $selVal , 'productID', 'productIMG', "UPDATE product SET productIMG = :productIMG WHERE productID = :productID", "backdexProductsUpdate&id=$id");
     }
 }
+
+
+
+
+
--- backdexPages/backdexProductsCreate.php
+++ backdexPages/backdexProductsCreate.php
@@ -3,48 +3,81 @@ require_once("./backdexPageControllers/backdexProductCreateController.php");
 ?>
 
 <div class="row" style="margin-top: 50px;">
-    <div class="card">
 
-        <div class="card-content">
-            <div class="row">
-                <form class="col s12" name="create" id="create" method="POST" action="./backdex.php?page=backdexProductsCreate&action=create">
-                    <div class="row">
-                        <div class="input-field col s12 m12">
-                            <textarea id="categoryID" name="categoryID" class="materialize-textarea" required></textarea>
-                            <label for="categoryID">Category ID</label>
+<?php
+switch ($productCase) {
+    default:?>
+        <div class="card">
+            <div class="card-content">
+                <div class="row">
+                    <form class="col s12" name="create" id="create" method="POST" action="./backdex.php?page=backdexProductsCreate&action=create">
+                        <div class="row">
+                            <div class="input-field col s12 m12">
+                                <textarea id="categoryID" name="categoryID" class="materialize-textarea" required></textarea>
+                                <label for="categoryID">Category ID</label>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s12 m12">
-                            <textarea id="productName" name="productName" class="materialize-textarea" required></textarea>
-                            <label for="productName">Product Name</label>
+                        <div class="row">
+                            <div class="input-field col s12 m12">
+                                <textarea id="productName" name="productName" class="materialize-textarea" required></textarea>
+                                <label for="productName">Product Name</label>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s12 m12">
-                            <textarea id="productDescription" name="productDescription" class="materialize-textarea" required></textarea>
-                            <label for="productDescription">Product Description</label>
+                        <div class="row">
+                            <div class="input-field col s12 m12">
+                                <textarea id="productDescription" name="productDescription" class="materialize-textarea" required></textarea>
+                                <label for="productDescription">Product Description</label>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s12 m12">
-                            <textarea id="productPrice" name="productPrice" class="materialize-textarea" required></textarea>
-                            <label for="productPrice">Product Price</label>
+                        <div class="row">
+                            <div class="input-field col s12 m12">
+                                <textarea id="productPrice" name="productPrice" class="materialize-textarea" required></textarea>
+                                <label for="productPrice">Product Price</label>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="input-field col s12 m12">
-                            <textarea id="productStock" name="productStock" class="materialize-textarea" required></textarea>
-                            <label for="productStock">Product stock</label>
+                        <div class="row">
+                            <div class="input-field col s12 m12">
+                                <textarea id="productStock" name="productStock" class="materialize-textarea" required></textarea>
+                                <label for="productStock">Product stock</label>
+                            </div>
                         </div>
-                    </div>
-                    <div class="row">
-                        <div class="card-action">
-                            <button class="waves-effect waves-light btn-large" type="submit" name="Submit" value="Submit">Save changes and chose product image</button>
+                        <div class="row">
+                            <div class="card-action">
+                                <button class="waves-effect waves-light btn-large" type="submit" name="Submit" value="Submit">Save changes and chose product image</button>
+                            </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
+<?php
+        break;
+    case "picture";
+
+    ?>
+
+<div class="card">
+    <div class="card-image">
+        <img src="./images/<?php echo $updateProduct['productIMG']?>">
+        <form name="imgup" method="post" action="./backdex.php?action=productImage&page=backdexProductsCreate&id=<?php echo $_GET['id'];?>" enctype="multipart/form-data">
+
+                <div class="card-content">
+                    <div class="file-field input-field">
+                    <div class="btn">
+                        <span>Change image</span>
+                        <input type="file" name="image" value="">
+                    </div>
+
+                    <div class="file-path-wrapper">
+                        <input class="file-path validate" type="text" placeholder="Select a file to upload">
+                    </div>
+                    </div>
+                    <button class="btn waves-effect waves-light" type="submit" name="submit" value="submit">Upload new image</button>
+                </div>
+
+        </form>
     </div>
-</div>
+
+
+
+<?PHP }
