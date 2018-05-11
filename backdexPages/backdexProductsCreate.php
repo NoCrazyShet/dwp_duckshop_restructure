5997e00c4a7cdb21e7fc067ea236080752f62073
Zeljko laptop <duranovic46@gmail.com>
2018-05-11 09:19:11 +0200

    Required fields in product creation

--- backdexPages/backdexProductsCreate.php
+++ backdexPages/backdexProductsCreate.php
@@ -4,46 +4,43 @@ require_once("./backdexPageControllers/backdexProductCreateController.php");
 
 <div class="row" style="margin-top: 50px;">
     <div class="card">
-        <div class="card-image">
-            <img src="./images/egg.jpg">
-        </div>
 
         <div class="card-content">
             <div class="row">
                 <form class="col s12" name="create" id="create" method="POST" action="./backdex.php?page=backdexProductsCreate&action=create">
                     <div class="row">
                         <div class="input-field col s12 m12">
-                            <textarea id="categoryID" name="categoryID" class="materialize-textarea"></textarea>
+                            <textarea id="categoryID" name="categoryID" class="materialize-textarea" required></textarea>
                             <label for="categoryID">Category ID</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12 m12">
-                            <textarea id="productName" name="productName" class="materialize-textarea"></textarea>
+                            <textarea id="productName" name="productName" class="materialize-textarea" required></textarea>
                             <label for="productName">Product Name</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12 m12">
-                            <textarea id="productDescription" name="productDescription" class="materialize-textarea"></textarea>
+                            <textarea id="productDescription" name="productDescription" class="materialize-textarea" required></textarea>
                             <label for="productDescription">Product Description</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12 m12">
-                            <textarea id="productPrice" name="productPrice" class="materialize-textarea"></textarea>
+                            <textarea id="productPrice" name="productPrice" class="materialize-textarea" required></textarea>
                             <label for="productPrice">Product Price</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="input-field col s12 m12">
-                            <textarea id="productStock" name="productStock" class="materialize-textarea"></textarea>
+                            <textarea id="productStock" name="productStock" class="materialize-textarea" required></textarea>
                             <label for="productStock">Product stock</label>
                         </div>
                     </div>
                     <div class="row">
                         <div class="card-action">
-                            <button class="waves-effect waves-light btn-large" type="submit" name="Submit" value="Submit">Create Product</button>
+                            <button class="waves-effect waves-light btn-large" type="submit" name="Submit" value="Submit">Save changes and chose product image</button>
                         </div>
                     </div>
                 </form>
