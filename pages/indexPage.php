5c723038ea98fc86ac6a22e8f718fe00dc65e0f1
NoCrazyShet <j.pettit51@gmail.com>
2018-05-21 14:09:21 +0200

    Added articlesubtitle to index news view

--- pages/indexPage.php
+++ pages/indexPage.php
@@ -10,7 +10,7 @@ $slider_classes = array("left-align", "center-align", "right-align");
                 <a href="./index.php?page=newsView&articleID=<?php echo $news['articleID'] ?>"><img src="./images/<?php echo $news['articleIMG'] ?>"></a>
                 <div class="caption <?php shuffle($slider_classes); $class = array_slice($slider_classes, 0, 2); echo $class[0]; ?>">
                     <h3><span class="teal" style="padding: 3px 10px 3px 10px;"><?php echo $news ['articleTitle'] ?></span></h3>
-                    <h5 class="light grey-text text-lighten-3"><span class="teal" style="padding: 5px 10px 5px 10px;">Here's our small slogan.</span></h5>
+                    <h5 class="light grey-text text-lighten-3"><span class="teal" style="padding: 5px 10px 5px 10px;"><?php echo $news['articleSubTitle']?></span></h5>
                 </div>
             </li>
         <?php } ?>
